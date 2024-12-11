7ed502cf9e451e24a9719be4b32ecdb1b9810bd5
Yashi Gupta <yashi.g23csai@nst.rishihood.edu.in>
2024-12-11 21:32:54 +0530

    / route

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Welcome to the homepage!")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('myapp.urls')),
+    path('', home, name='home'),
 ]
